d9fb7c375b4d12ae4c2407eaa89c1ade68bafd77
rxestrella <rxestrella@gmail.com>
2019-12-09 23:10:03 -0800

    serving page but cannot render

--- server.py
+++ server.py
@@ -1,37 +1,38 @@
 import eel
 import serial
+eel.init('web')
 
-# port = "/dev/tty.usbserial-00000000"
-# cereal = serial.Serial(port, 9600)
+port = "COM6"
+cereal = serial.Serial(port, 9600)
 
-@eel.expose
 def readMhorcel():
     #this is the function to handle the serial thread
-    #global cereal
+    global cereal
     while True:
-        data = 'hi'#cereal.read(256)
+        data = cereal.read(256)
         if len(data) > 0:
             print(data)
             eel.renderData(data)
-        sleep(1)
+        eel.sleep(5.0)
 
+@eel.expose
 def sendMhorcel(data):
-    #global cereal
+    global cereal
     try:
-        print('hello')
-        #cereal.write(data)
+        cereal.write(data)
     except:
         print('an error occurred')
 
-eel.init('web')
+
 
 options = {
     'host': 'localhost',
-    'port': 80,
+    'port': 8080,
     'block': False
 }
 
-eel.start('web/main.html', options)
+eel.spawn(readMhorcel)
+eel.start('main.html', options)
 
 while True:
-    eel.sleep(10);
+    eel.sleep(2.0)
